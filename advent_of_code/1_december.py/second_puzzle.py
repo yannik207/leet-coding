63d639111f65a68a611c3cbd572e7cd825cac1d7
Yannik Sacherer <A1DF6EF@jwpmx2kh4d.koeln.ads.risnet.de>
2024-12-02 20:52:24 +0100

    fixed a typo

--- advent_of_code/1_december.py/second_puzzle.py
+++ advent_of_code/1_december.py/second_puzzle.py
@@ -1,11 +1,11 @@
 from util import text_file_opener
 left_list, right_list = text_file_opener('advent_of_code/1_december.py/input.txt')
 
-occuranc_scor = 0
+occuranc_score = 0
 
 for i in left_list:
     cou = right_list.count(i)
-    occuranc_scor += (cou * i)
+    occuranc_score += (cou * i)
 
 print(f"the occurance score is: {occuranc_score}")
     
